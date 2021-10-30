003372da0f4b117391c0df7815a94cef4486084d
Sebastian Deymonnaz <adeymonn@hotmail.com>
2021-10-30 11:14:52 -0300

    boton resumir corregido

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 
-let contenedor = document.getElementById('contenedorPpal');
+let categoria = document.getElementById('inputCategoria');
+let cantidad = document.getElementById('inputCantidad');
+let resumen = document.getElementById('cajaTotal');
+let precio = 200;
 
-console.log(contenedor);
+
+function calcularPrecio() {
+    if (categoria.value == 'Estudiante') {
+        precio = precio * 0.8;
+    }
+    else if (categoria.value == 'Trainee') {
+        precio = precio * 0.5
+    } else{
+        precio = precio * 0.15
+    }
+        resumen.textContent = `Total a Pagar: $${cantidad.value * precio}`;
+}
+
+
+
+
+
+
+
